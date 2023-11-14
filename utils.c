a1a66f52a231128c9308f84386e682b11dfe6204
LillishoT <140070350+LillishoT@users.noreply.github.com>
2023-11-14 10:55:17 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * is_printable - Evaluates if a char is printable
-* By LilyM and AmanuelM
+* By LilyM and AmanuelM edited by LilyM
 * @c: Char to be evaluated.
 *
 * Return: 1 if c is printable, 0 otherwise
