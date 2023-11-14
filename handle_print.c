f537168cb10b20baa49988b8837e50a9252bdf59
LillishoT <140070350+LillishoT@users.noreply.github.com>
2023-11-14 10:52:16 +0300

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * handle_print - Prints an argument based on its type
-* By LilyM and AmanuelM
+* By LilyM and AmanuelM Edited by LilyM
 * @fmt: Formatted string in which to print the arguments.
 * @list: List of arguments to be printed.
 * @ind: ind.
